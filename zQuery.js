e335b4f5286602d5207aaa0f1217725926541cb0
znm <znm_92@163.com>
2016-08-09 18:30:20 +0800

    completed query selector function

--- zQuery.js
+++ zQuery.js
@@ -23,152 +23,174 @@
 //
 // })(window);
 
-(function(window, undefined){
+(function(globalEnv){
 
-  window.zQuery = window.$ = function(selector) {
-    // selector可为： body，html标签，html字符串，html元素id，选择器表达式
-    if (typeof selector !== 'string' || selector === '') {
-      throw new Error('Expected STRING as selector.')
+  // 根据［单个］选择器字符串查询，返回目标元素下［所有］符合的元素
+  // @param {string} selector "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的［单个］查询字符串
+  // @param {object=} root 提供时以其为遍历起点，否则以document为起点
+  // @return {array.<node>} 返回成员类型为node的数组或空数组
+  var singleSelectorAllMatches = function(selector, root) {
+    var _root;
+    switch (root) {
+      case undefined:
+        _root = document.documentElement;
+        break;
+      case null:
+        return [];
+      default:
+        _root = root;
     }
-  }
+    var walker = document.createTreeWalker(_root, NodeFilter.SHOW_ELEMENT, null, false);
+    var currentNode = walker.nextNode();
+    var result = [];
+    switch (true) {
 
-  Node.prototype.hasClass = function(className) {
-    if (typeof className !== 'string') {
-      throw new Error('Expected STRING as class name.')
+      case /^#([\w-]+)$/.test(selector):
+        var tarId = RegExp.$1;
+        while (currentNode !== null) {
+          if (currentNode.id === tarId) {
+            result.push(currentNode);
+            break;
+          }
+          currentNode = walker.nextNode();
+        }
+        break;
+
+      case /^\.[\w-\.]+$/.test(selector):
+        var tarClasses = selector.split('.');
+        while (currentNode !== null) {
+          var thisNodeMatches = true;
+          for (var i in tarClasses) {
+            if (!currentNode.hasClass(tarClasses[i])) {
+              thisNodeMatches = false;
+              break;
+            }
+          }
+          if (thisNodeMatches) {
+            result.push(currentNode);
+          }
+          currentNode = walker.nextNode();
+        }
+        break;
+
+      case /^([\w-]+)$/.test(selector):
+        var tarTag = RegExp.$1;
+        while (currentNode !== null) {
+          if (currentNode.nodeName === tarTag.toUpperCase()) {
+            result.push(currentNode);
+          }
+          currentNode = walker.nextNode();
+        }
+        break;
+
+      case /^\[([\w-]+)\]$/.test(selector):
+        var tarAttr = RegExp.$1;
+        while (currentNode !== null) {
+          if (currentNode.hasAttribute(tarAttr)) {
+            result.push(currentNode);
+          }
+          currentNode = walker.nextNode();
+        }
+        break;
+
+      case /^\[([\w-]+)=([\w-]+)\]$/.test(selector):
+        var tarAttr = RegExp.$1;
+        var tarValue = RegExp.$2;
+        while (currentNode !== null) {
+          if (currentNode.getAttribute(tarAttr) === tarValue) {
+            result.push(currentNode);
+          }
+          currentNode = walker.nextNode();
+        }
+        break;
+
+      default:
+        throw new Error('Function "queryAll" accepts only 1 selector.')
+
     }
-    if (className === '') {
-      return true;
+    return result;
+  };
+
+  // 根据［单个］选择器字符串查询，返回目标元素下［第一个］符合的元素
+  // @param {string} selector "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的［单个］查询字符串
+  // @param {object=} root 提供时以其为遍历起点，否则以document为起点
+  // @return {object|null} 返回node对象或null
+  var singleSelectorOneMatch = function(selector, root) {
+    return singleSelectorAllMatches(selector, root)[0] || null;
+  };
+
+  // 根据［组合］选择器字符串查询，返回目标元素下［第一个］符合的元素
+  // @param {string} selectorGroup "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的[以空格连接的］查询字符串
+  // @param {object=} root 提供时以其为遍历起点，否则以document为起点
+  // @return {object|null} 返回node对象或null
+  var multipleSelectorOneMatch = function(selectorGroup, root) {
+    if (typeof selectorGroup !== 'string' || selectorGroup === '') {
+      throw new Error('Expected STRING as selector.');
     }
-    var currentClasses = this.getAttribute('class');
-    if (currentClasses !== null) {
-      if (currentClasses.indexOf(className) !== -1) {
+    var selectorArr = selectorGroup.split(' ');
+    switch (selectorArr.length) {
+      case 1:
+        return singleSelectorOneMatch(selectorArr[0], root);
+      case 2:
+        var r1 = singleSelectorOneMatch(selectorArr[0]);
+        return singleSelectorOneMatch(selectorArr[1], r1);
+      case 3:
+        var r1 = singleSelectorOneMatch(selectorArr[0]);
+        var r2 = singleSelectorOneMatch(selectorArr[1], r1);
+        return singleSelectorOneMatch(selectorArr[2], r2);
+      case 4:
+        var r1 = singleSelectorOneMatch(selectorArr[0]);
+        var r2 = singleSelectorOneMatch(selectorArr[1], r1);
+        var r3 = singleSelectorOneMatch(selectorArr[2], r2);
+        return singleSelectorOneMatch(selectorArr[3], r3);
+      default:
+        throw new Error('Expected at most 4 selector snippets.')
+    }
+  };
+
+  globalEnv.queryOne = function(selectorGroup) {
+    return multipleSelectorOneMatch(selectorGroup, document.documentElement);
+  };
+
+  globalEnv.queryAll = function(selector) {
+    return singleSelectorAllMatches(selector, document.documentElement);
+  };
+
+  // 为Node原型添加方法
+  (function(nodePrototype) {
+
+    nodePrototype.hasClass = function(className) {
+      if (typeof className !== 'string') {
+        throw new Error('Expected STRING as class name.')
+      }
+      if (className === '') {
         return true;
       }
+      var currentClasses = this.getAttribute('class');
+      if (currentClasses !== null) {
+        if (currentClasses.indexOf(className) !== -1) {
+          return true;
+        }
+      }
+      return false;
     }
-    return false;
-  }
+
+    nodePrototype.queryOne = function(selectorGroup){
+      return multipleSelectorOneMatch(selectorGroup, this);
+    };
+
+    nodePrototype.queryAll = function(selector){
+      return singleSelectorAllMatches(selector, this);
+    };
+
+
+  })(globalEnv.Node.prototype);
+
 
 
 })(window);
 
-// 根据单个选择器字符串查询，返回目标元素下［所有］符合的元素
-// @param {string} selector "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的单个查询字符串
-// @param {object=} root 提供时以其为遍历起点，否则以document为起点
-// @return {array.<node>} 返回成员类型为node的数组或空数组
-var singleSelectorAllMatches = function(selector, root) {
-  var _root;
-  switch (root) {
-    case undefined:
-      _root = document;
-      break;
-    case null:
-      return [];
-    default:
-      _root = root;
-  }
-  var walker = document.createTreeWalker(_root, NodeFilter.SHOW_ELEMENT, null, false);
-  var currentNode = walker.nextNode();
-  var result = [];
-  switch (true) {
 
-    case /^#([\w-]+)$/.test(selector):
-      var idMatch = _root.getElementById(RegExp.$1);
-      if (idMatch) {
-        result.push(idMatch);
-      }
-      break;
-
-    case /^\.[\w-\.]+$/.test(selector):
-      var tarClasses = selector.split('.');
-      while (currentNode !== null) {
-        var thisNodeMatches = true;
-        for (var i in tarClasses) {
-          if (!currentNode.hasClass(tarClasses[i])) {
-            thisNodeMatches = false;
-            break;
-          }
-        }
-        if (thisNodeMatches) {
-          result.push(currentNode);
-        }
-        currentNode = walker.nextNode();
-      }
-      break;
-
-    case /^([\w-]+)$/.test(selector):
-      var tarTag = RegExp.$1;
-      while (currentNode !== null) {
-        if (currentNode.nodeName === tarTag.toUpperCase()) {
-          result.push(currentNode);
-        }
-        currentNode = walker.nextNode();
-      }
-      break;
-
-    case /^\[([\w-]+)\]$/.test(selector):
-      var tarAttr = RegExp.$1;
-      while (currentNode !== null) {
-        if (currentNode.hasAttribute(tarAttr)) {
-          result.push(currentNode);
-        }
-        currentNode = walker.nextNode();
-      }
-      break;
-
-    case /^\[([\w-]+)=([\w-]+)\]$/.test(selector):
-      var tarAttr = RegExp.$1;
-      var tarValue = RegExp.$2;
-      while (currentNode !== null) {
-        if (currentNode.getAttribute(tarAttr) === tarValue) {
-          result.push(currentNode);
-        }
-        currentNode = walker.nextNode();
-      }
-      break;
-
-    default:
-      throw new Error('Invalid selector string.')
-
-  }
-  return result;
-}
-
-// 根据单个选择器字符串查询，返回目标元素下［第一个］符合的元素
-// @param {string} selector "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的单个查询字符串
-// @param {object=} root 提供时以其为遍历起点，否则以document为起点
-// @return {object|null} 返回node对象或null
-var singleSelectorFisrtMatch = function(selector, root) {
-  return singleSelectorAllMatches(selector, root)[0] || null;
-}
-
-var query = function(selector) {
-  if (typeof selector !== 'string' || selector === '') {
-    throw new Error('Expected STRING as selector.')
-  }
-  var selectors = selector.split('/\s+/');
-  switch (selectors.length) {
-    case 1:
-      r1 = singleSelectorFisrtMatch(selectors[0]);
-    case 2:
-      r2 = singleSelectorFisrtMatch
-    default:
-
-  }
-
-  var r1 = singleSelectorFisrtMatch(selectors[0]);
-  if (length === 1) {
-    return r1;
-  } else if (length === 2) {
-      if (r1 !== undefined) {
-      var r2 = singleSelectorFisrtMatch(selectors[1], r1)
-    }
-  }
-
-  // var r = selectors.reduce(function(s1, s2) {
-  //   return singleSelectorFisrtMatch(s2, s1);
-  // }, document.body)
-  // console.log(r)
-}
-
-// console.log(document.getElementById('test').hasClass('fuck'))
-console.log(query('#test'))
+// console.log(body.getElementById('test').hasClass('fuck'))
+console.log(document.body.queryAll('#dick .fuck'))
+console.log(queryAll('.fuck.shit'))
