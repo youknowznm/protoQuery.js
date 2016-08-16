85c9bd9efbd953eaaf166b3bd559bbdabc6d1fa3
youknowznm <znm_92@163.com>
2016-08-16 23:10:36 +0800

    fixed an issue (#01) TreeWalker实例在遍历时并不会计算根节点，因此在这里添加对根节点的判定

--- zQuery.js
+++ zQuery.js
@@ -9,7 +9,7 @@
     if (!tarNode instanceof Node) {
       throw new Error('Expected NODE as target node.');
     }
-    if (typeof selector !== 'string' ) {
+    if (typeof selector !== 'string' || selector === '') {
       throw new Error('Expected STRING as selector.');
     }
     switch (true) {
@@ -46,20 +46,17 @@
   // @param {string} selector "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的［单个］查询字符串
   // @param {object.node=} root 提供时以其为遍历起点，否则以document为起点
   // @return {array.<node>} 返回成员类型为node的数组或空数组
-  var singleSelectorAllMatches = function(selector, root) {
-    var _root;
-    switch (root) {
-      case undefined:
-        _root = document.documentElement;
-        break;
-      case null:
-        return [];
-      default:
-        _root = root;
+  var singleSelectorAllResults = function(selector, root) {
+    if (root === undefined) {
+      return [];
     }
-    var walker = document.createTreeWalker(_root, NodeFilter.SHOW_ELEMENT, null, false);
-    var currentNode = walker.nextNode();
     var result = [];
+    // #01 TreeWalker实例在遍历时并不会计算根节点，因此在这里添加对根节点的判定
+    if (nodeMatchesSelector(root, selector)) {
+      result.push(root);
+    }
+    var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, null, false);
+    var currentNode = walker.nextNode();
     while (currentNode !== null) {
       if (nodeMatchesSelector(currentNode, selector)) {
         result.push(currentNode);
@@ -69,36 +66,34 @@
     return result;
   };
 
-  // 根据［单个］选择器字符串查询，返回目标元素下［第一个］符合的元素
-  // @param {string} selector "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的［单个］查询字符串
-  // @return {object.node|null} 返回node对象或null
-  var singleSelectorOneMatch = function(selector, root) {
-    return singleSelectorAllMatches(selector, root)[0] || null;
-  };
+  // // 根据［单个］选择器字符串查询，返回目标元素下［第一个］符合的元素
+  // // @param {string} selector "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式的［单个］查询字符串
+  // // @return {object.node|null} 返回node对象或null
+  // var singleSelectorOneMatch = function(selector, root) {
+  //   return singleSelectorAllResults(selector, root)[0] || null;
+  // };
 
   // 根据［组合］选择器字符串查询，返回目标元素下［所有］符合的元素
   // @param {string} selectorGroup "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式以空格连接的查询字符串
   // @return {array.<node>} 返回成员类型为node的数组或空数组
-  var groupSelectorAllMatches = function(selectorGroup, root) {
-    if (typeof selectorGroup !== 'string' || selectorGroup === '') {
-      throw new Error('Expected STRING as selector.');
-    }
+  var groupSelectorAllResults = function(selectorGroup, root) {
     var selectorArr = selectorGroup.split(' ');
     switch (selectorArr.length) {
       case 1:
-        return singleSelectorAllMatches(selectorArr[0], root);
+        return singleSelectorAllResults(selectorArr[0], root);
       case 2:
-        var r1 = singleSelectorOneMatch(selectorArr[0]);
-        return singleSelectorAllMatches(selectorArr[1], r1);
+        var r1 = singleSelectorAllResults(selectorArr[0], root)[0];
+        console.log(r1)
+        return singleSelectorAllResults(selectorArr[1], r1);
       case 3:
-        var r1 = singleSelectorOneMatch(selectorArr[0]);
-        var r2 = singleSelectorOneMatch(selectorArr[1], r1);
-        return singleSelectorAllMatches(selectorArr[2], r2);
+        var r1 = singleSelectorAllResults(selectorArr[0], root)[0];
+        var r2 = singleSelectorAllResults(selectorArr[1], r1)[0];
+        return singleSelectorAllResults(selectorArr[2], r2);
       case 4:
-        var r1 = singleSelectorOneMatch(selectorArr[0]);
-        var r2 = singleSelectorOneMatch(selectorArr[1], r1);
-        var r3 = singleSelectorOneMatch(selectorArr[2], r2);
-        return singleSelectorAllMatches(selectorArr[3], r3);
+        var r1 = singleSelectorAllResults(selectorArr[0], root)[0];
+        var r2 = singleSelectorAllResults(selectorArr[1], r1)[0];
+        var r3 = singleSelectorAllResults(selectorArr[2], r2)[0];
+        return singleSelectorAllResults(selectorArr[3], r3);
       default:
         throw new Error('Expected at most 4 selector snippets.')
     }
@@ -112,7 +107,7 @@
   // @param {string} selectorGroup "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式以空格连接的查询字符串
   // @return {array.<node>} 返回成员类型为node的数组或空数组
   globalEnv.$ = globalEnv.query = function(selectorGroup) {
-    return groupSelectorAllMatches(selectorGroup, document.documentElement);
+    return groupSelectorAllResults(selectorGroup, document.documentElement);
   };
 
   // 在文档渲染结束、即将加载内嵌资源时，执行指定函数
@@ -148,7 +143,7 @@
     // @param {string} selectorGroup "#header"，".item"，"ul"，"[type]"，"[type=radio]"形式以空格连接的查询字符串
     // @return {array.<node>} 返回成员类型为node的数组或空数组
     nodePrototype.$ = nodePrototype.query = function(selectorGroup){
-      return groupSelectorAllMatches(selectorGroup, this);
+      return groupSelectorAllResults(selectorGroup, this);
     };
 
     // 获取或设置目标属性
@@ -190,4 +185,5 @@
   })(globalEnv.Array.prototype);
 })(window);
 
-console.log(document.body.query('body #dick .fuck.shit')[0].attr('f', 'f'))
+// console.log(nm('.fuck.shit', document.documentElement));
+console.log(query('body #dick .shit.fuck'))
