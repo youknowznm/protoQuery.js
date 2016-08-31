958b01452e92875b657509a18534ad9667a56fec
youknowznm <znm_92@163.com>
2016-08-31 21:10:29 +0800

    updated style transformation

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function showTar(e){
 }
 
 // var t = query('#black')[0].on('click mouseover', '#black', showTar);
-var t = query('#black')[0]
-// tg(t, 'opacity', 0.1)
+var t = query('#black')[0];
 
-t.css('opacity', 0.1)
+console.log(t.show('transform'))
